27e92b44af17135506b27ee435d67ffdfe979d96
Regan Tosh <toshregan94@gmail.com>
2026-04-12 12:45:46 +0300

    changes

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // Write your solution in this file!
 
-const burgers=[`Hamburgers`, `Cheeseburger`];
-let featuredDrink=[`Strawberry Milkshake`];
+const burgers=("Hamburgers", "Cheeseburger");
+let featuredDrink=("Strawberry Milkshake");
 
 // function scoped variables
 function addBurger(){
     const newBurger= `Flatburger`;
     burgers.push(newBurger);
+    addBurger();
 
     // block scope variable
     if(true) {
-        const anotherNewBurger= `Maple Bacon Burger`;
+        const anotherNewBurger= "Maple Bacon Burger";
         burgers.push(anotherNewBurger);
 
 
     }
     function changeFeaturedDrink(){
-        featuredDrink= `The Javashake`;
+        featuredDrink= "The Javashake";
 
     }
+    changeFeaturedDrink();
 }
