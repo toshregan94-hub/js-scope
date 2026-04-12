1e0407f4488b2bb2497e11bbdf0d0b15bd9c6fdb
Regan Tosh <toshregan94@gmail.com>
2026-04-12 12:28:46 +0300

    New changes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+let burgers=[`Hamburgers`, `Cheeseburger`];
+let featuredDrink=[`Strawberry Milkshake`];
+
+// function scoped variables
+function addBurger(){
+    let newBurger= `Flatburger`;
+    burgers.push(newBurger);
+
+    // block scope variable
+    if(true) {
+        let anotherNewBurger= `Maple Bacon Burger`;
+        burgers.push(anotherNewBurger);
+
+
+    }
+    function changeFeaturedDrink(){
+        featuredDrink= `The Javashake`;
+
+    }
+}
